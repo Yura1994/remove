c116c08c95611a531c946f18375b666f193361aa
Yura <61066244+Yura1994@users.noreply.github.com>
2020-06-11 14:20:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function myFunction_1() {
+     var sel = document.getElementById("remove_1");
+     sel.remove();
+};
+function myFunction_2() {
+    var sel = document.getElementById("remove_2");
+    sel.remove();
+};
+function myFunction_3() {
+    var sel = document.getElementById("remove_3");
+    sel.remove();
+};
